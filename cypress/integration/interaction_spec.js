737357f1652f90b3537e14274840162b03b188b5
Christian Fei <crifei93@gmail.com>
2017-09-11 22:16:54 +0200

    clean test

--- cypress/integration/interaction_spec.js
+++ cypress/integration/interaction_spec.js
@@ -5,16 +5,16 @@ describe('Interaction', function () {
 
   it('clicking the button adds a result', function () {
     cy.contains('Trigger!').click()
-    cy.get('li').contains('42')
     cy.get('ul > li').as('items')
+    cy.get('li').contains('42')
     cy.get('@items').should('have.length', 1)
   })
 
   it('clicking on the button again adds another result ', function () {
     cy.contains('Trigger!').click()
+    cy.get('ul > li').as('items')
     cy.get('li').contains('42')
     cy.contains('Trigger!').click()
-    cy.get('ul > li').as('items')
     cy.get('@items').should('have.length', 2)
   })
 })
